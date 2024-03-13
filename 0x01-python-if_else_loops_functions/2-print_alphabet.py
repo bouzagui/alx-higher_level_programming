48f9110aafe792b0bdf85fac58952c7eaa3005bd
bouzagui <badrbouzagui@gmail.com>
2024-03-13 02:43:10 +0000

    Done

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
 ascii_start = 97
 ascii_end = 122
-ascii_output = ""
 
 for ascii_value in range(ascii_start, ascii_end + 1):
-    ascii_output = ascii_output + chr(ascii_value)
-
-print("{}".format(ascii_output), end="")
+    print("{}".format(chr(ascii_value)), end="")
