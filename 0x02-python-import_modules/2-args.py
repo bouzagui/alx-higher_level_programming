718fd27775a2efcfee866a8305bf6cddbb775d66
bouzagui <badrbouzagui@gmail.com>
2024-03-14 16:33:01 +0000

    The third tasks are done ✅

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,7 +4,7 @@ import sys
 if __name__ == '__main__':
     num_args = len(sys.argv) - 1
     if num_args == 0:
-        print("{} argument.".format('0'))
+        print("{} arguments.".format('0'))
     elif num_args == 1:
         print("{} argument:".format(num_args))
     else:
