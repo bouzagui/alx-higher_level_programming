9db8f1f775e50aa13e76fc16492ef54183723756
bouzagui <badrbouzagui@gmail.com>
2024-03-14 02:35:22 +0000

    All tasks are done

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for i in range(25, -1, -1):
-    memory = i + 65
+    mem = i + 65
     if i % 2 == 1:
-        memory += 32
-    print("{:memory}".format(memory), end="")
+        mem = mem + 32
+    print("{:mem}".format(mem), end="")
