8c731b35e78dd72429f6549166430e1896f3c763
bouzagui <badrbouzagui@gmail.com>
2024-03-17 16:38:58 +0000

    this is me

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    for mat in matrix:
+        for number in mat:
+            if mat[len(mat) - 1] != number:
+                print("{:d}".format(number), end=' ')
+            else:
+                print("{:d}".format(number), end='')
+                print()
+
+            
---
+++ 0x03-python-data_structures/test.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+str = "hello"
+my_list = [98, 55, 44, 54, 12, 8]
+print(my_list[5])
