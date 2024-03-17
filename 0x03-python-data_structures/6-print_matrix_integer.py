aedcfe3f662807b062522f5424356e55e2f41718
bouzagui <badrbouzagui@gmail.com>
2024-03-17 16:51:30 +0000

    [A[A[B[B

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -7,11 +7,3 @@ def print_matrix_integer(matrix=[[]]):
                 print()
             else:
                 print("{:d}".format(number), end=' ')
-
-
-
-def print_matrix_integer(matrix=[[]]):
-    for mat in matrix:
-        for element in mat:
-            print("{:d}".format(element), end="" if mat[len(mat) - 1] == element else " ")
-        print()
