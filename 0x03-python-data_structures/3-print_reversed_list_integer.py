6ce6d1f868ccf332eb3befb4081d9a9768a593ee
bouzagui <badrbouzagui@gmail.com>
2024-03-17 13:46:01 +0000

    fixed return value for empty list

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -2,7 +2,7 @@
 def print_reversed_list_integer(my_list=[]):
     reversed_list = my_list[::-1]
     if not reversed_list:
-        return None
+        return my_list
     idx = 0
     while idx < len(reversed_list):
         print("{:d}".format(reversed_list[idx]))
