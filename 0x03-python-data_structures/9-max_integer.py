f14a2f65161f9a079e57d802444a7aaa477a0b79
bouzagui <badrbouzagui@gmail.com>
2024-03-18 03:15:16 +0000

    Done

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if not my_list:
+        return None
+    max_num = my_list[0]
+    for number in my_list[1:]:
+        if number > max_num:
+            max_num = number
+    return max_num
