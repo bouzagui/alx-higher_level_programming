2d94787f9f19f9f0fafee6519fa5dcc31cd94a60
bouzagui <badrbouzagui@gmail.com>
2024-03-20 20:31:05 +0000

    🥳✅

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    for i in enumerate(my_list):
-        return list(map(lambda i: i if i != search else replace, my_list))
+    return list(map(lambda i: i if i != search else replace, my_list))
