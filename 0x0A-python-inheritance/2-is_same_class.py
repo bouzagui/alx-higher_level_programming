bbd656ea8da0f2a9232c7dc8801b9c0faa51f38c
bouzagui <badrbouzagui@gmail.com>
2024-04-16 02:22:11 +0100

    Done

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+
 def is_same_class(obj, a_class):
 
     return type(obj).__name__ == a_class.__name__
--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+""" defined a function that returns a string """
+
+
 def is_kind_of_class(obj, a_class):
+    """ returns true if the class is a kind of class """
     try:
         if isinstance(obj, a_class):
             return True
