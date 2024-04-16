3252bca892c57671d3e705eebab5f458286f4db7
bouzagui <badrbouzagui@gmail.com>
2024-04-16 03:57:16 +0100

    Done

---
+++ 0x0A-python-inheritance/100-my_init.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" defined a class """
+
+class MyInt:
+    """ class """
+    def __init__(self, value):
+        return super().__ne__(value)
+    
+    def __ne__(self, value):
+        return super().__init__(value)
