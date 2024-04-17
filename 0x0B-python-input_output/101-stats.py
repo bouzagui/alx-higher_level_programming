97df61c8373bac0178c9b42c468602a3dff64c52
bouzagui <badrbouzagui@gmail.com>
2024-04-17 03:13:08 +0100

    Done ✅

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,40 +1,40 @@
 #!/usr/bin/python3
-""" module """
-
+#!/usr/bin/python3
+"""Print stats module"""
 import sys
 
 
-counter = 0
-data = {"File size": 0, "200": 0, "301": 0, "400": 0,
-        "401": 0, "403": 0, "404": 0, "405": 0, "500": 0}
-globaldata = []
+def print_stats(total_size, status_codes):
+    """
+    Print statistics.
+    """
+    print("File size: {}".format(total_size))
+    for code in sorted(status_codes):
+        print("{}: {}".format(code, status_codes[code]))
 
 
-try:
-    for line in sys.stdin:
-        if counter == 10:
-            counter = 0
-            for key, value in globaldata[0].items():
-                if value == 0:
-                    continue
-                print(key+": "+str(value))
-                sys.stdout.flush()
-        counter += 1
-        if counter == 1:
-            globaldata.insert(0, data.copy())
-        line = line.rstrip()
-        globaldata[0]["File size"] += int(line.split()[-1])
-        globaldata[0][line.split()[-2]] += 1
-    if counter > 0:
-        for key, value in globaldata[0].items():
-            if value == 0:
-                continue
-            print(key+": "+str(value))
-            sys.stdout.flush()
-except KeyboardInterrupt as e:
-    for key, value in globaldata[0].items():
-        if value == 0:
-            continue
-        print(key+": "+str(value))
-        sys.stdout.flush()
-    print(e)
+if __name__ == "__main__":
+    total_size = 0
+    status_codes = {
+         '200': 0, '301': 0, '400': 0, '401': 0,
+         '403': 0, '404': 0, '405': 0, '500': 0
+     }
+    line_count = 0
+
+    try:
+        for line in sys.stdin:
+            parts = line.split()
+            if len(parts) > 2:
+                total_size += int(parts[-1])
+                status_code = parts[-2]
+                if status_code in status_codes:
+                    status_codes[status_code] += 1
+
+            line_count += 1
+
+            if line_count % 10 == 0:
+                print_stats(total_size, status_codes)
+
+    except KeyboardInterrupt:
+        print_stats(total_size, status_codes)
+        raise
