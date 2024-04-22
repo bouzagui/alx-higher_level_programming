c25bdfa6ee31c1a558bd11447496bef5eec3f276
bouzagui <badrbouzagui@gmail.com>
2024-04-22 22:21:28 +0100

    Done ✅

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 """ modules """
-import os
-import sys
-import json
+
+
 class Base:
+    """ Base class for all modules """
     __nb_objects = 0
+
     def __init__(self, id=None):
         if id is not None:
             self.id = id
--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
+""" module for creating and updating objects from different sources """
 from models.base import Base
 
 class Rectangle(Base):
+    """ documentation """
     def __init__(self, width, height, x=0, y=0, id=None):
         self.__width = width
         self.__height = height
@@ -11,10 +13,12 @@ class Rectangle(Base):
     
     @property
     def width(self):
+        """ returns the width of attribute"""
         return self.__width
     
     @width.setter
     def width(self, value):
+        """ documents attribute """
         if not isinstance(value, int):
             raise TypeError('width must be an integer')
         if value <= 0:
@@ -22,10 +26,12 @@ class Rectangle(Base):
 
     @property
     def height(self):
+        """ returns the height of attribute """
         return self.__height
     
     @height.setter
     def height(self, value):
+        """ documents attribute """
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value <= 0:
@@ -33,16 +39,19 @@ class Rectangle(Base):
 
     @property
     def x(self):
+        """ returns the x coordinate """
         return self.__x
 
     @x.setter
     def x(self, value):
+        """ sets the x coordinate to the value of the current value """
         if value < 0:
             raise ValueError("x must be >= 0")
         self.__x = value
     
     @property
     def y(self):
+        """ returns the y coordinate """
         return self.__y
     
     @y.setter
@@ -53,30 +62,3 @@ class Rectangle(Base):
 
     def area(self):
         return self.__width * self.__height
-#!/usr/bin/python3
-""" 2-main """
-from models.rectangle import Rectangle
-
-if __name__ == "__main__":
-
-    try:
-        Rectangle(10, "2")
-    except Exception as e:
-        print("[{}] {}".format(e.__class__.__name__, e))
-
-    try:
-        r = Rectangle(10, 2)
-        r.width = -10
-    except Exception as e:
-        print("[{}] {}".format(e.__class__.__name__, e))
-
-    try:
-        r = Rectangle(10, 2)
-        r.x = {}
-    except Exception as e:
-        print("[{}] {}".format(e.__class__.__name__, e))
-
-    try:
-        Rectangle(10, 2, 3, -1)
-    except Exception as e:
-        print("[{}] {}".format(e.__class__.__name__, e))
