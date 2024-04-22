30006c033c7c4d878f5fa44f6d7663fcbb883540
bouzagui <badrbouzagui@gmail.com>
2024-04-22 23:32:07 +0100

    Done ✅

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -12,43 +12,61 @@ class Rectangle(Base):
         self.__x = x
         self.__y = y
         super().__init__(id)
-    
+
     @property
+
     def width(self):
         """ returns the width of attribute"""
         return self.__width
-    
+
     @width.setter
     def width(self, value):
-        """ doct """
-        self.__width = value
+        """ documents attribute """
+        if not isinstance(value, int):
+            raise TypeError('width must be an integer')
+        if value <= 0:
+            raise ValueError('width must be > 0')
 
     @property
     def height(self):
-        """ returns the height of attribute"""
+        """ returns the height of attribute """
         return self.__height
-    
+
     @height.setter
     def height(self, value):
-        """ doct """
+        """ documents attribute """
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value <= 0:
+            raise ValueError("height must be > 0")
         self.__height = value
-    
+
     @property
     def x(self):
-        """ returns the x of attribute"""
+        """ returns the x coordinate """
         return self.__x
 
     @x.setter
     def x(self, value):
-        """ doct """
+        """ documents attribute """
+        if not isinstance(value, int):
+            raise TypeError("x must be an integer")
+        if value < 0:
+            raise ValueError("x must be >= 0")
         self.__x = value
 
     @property
     def y(self):
-        """ returns the y of attribute"""
+        """ returns the y coordinate """
         return self.__y
 
     @y.setter
     def y(self, value):
-        """ doct """
+        """ documents attribute """
+        if value < 0:
+            raise ValueError("y must be >= 0")
         self.__y = value
+
+    def area(self):
+        """ returns the area of the rectangle """
+        return self.__width * self.__height
