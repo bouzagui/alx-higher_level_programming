2f9b8ba98b7b1d47ad3e0e73e32a5109ba57a5a5
bouzagui <badrbouzagui@gmail.com>
2024-04-23 03:58:28 +0100

    Done ✅

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,42 +1,53 @@
 #!/usr/bin/python3
-""" module for creating and updating objects from different sources """
+"""Defines a rectangle class."""
 from models.base import Base
 
 
 class Rectangle(Base):
-    """
-    arguments for creating and updating objects from different sources
-    """
+    """Represent a rectangle."""
+
     def __init__(self, width, height, x=0, y=0, id=None):
-        self.__width = width
-        self.__height = height
-        self.__x = x
-        self.__y = y
+        """Initialize a new Rectangle.
+
+        Args:
+            width (int): The width of the new Rectangle.
+            height (int): The height of the new Rectangle.
+            x (int): The x coordinate of the new Rectangle.
+            y (int): The y coordinate of the new Rectangle.
+            id (int): The identity of the new Rectangle.
+        Raises:
+            TypeError: If either of width or height is not an int.
+            ValueError: If either of width or height <= 0.
+            TypeError: If either of x or y is not an int.
+            ValueError: If either of x or y < 0.
+        """
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
         super().__init__(id)
 
     @property
     def width(self):
-        """ returns the width of attribute"""
+        """Set/get the width of the Rectangle."""
         return self.__width
 
     @width.setter
     def width(self, value):
-        """ documents attribute """
-        if not isinstance(value, int):
-            raise TypeError('width must be an integer')
+        if type(value) != int:
+            raise TypeError("width must be an integer")
         if value <= 0:
-            raise ValueError('width must be > 0')
+            raise ValueError("width must be > 0")
         self.__width = value
 
     @property
     def height(self):
-        """ returns the height of attribute """
+        """Set/get the height of the Rectangle."""
         return self.__height
 
     @height.setter
     def height(self, value):
-        """ documents attribute """
-        if not isinstance(value, int):
+        if type(value) != int:
             raise TypeError("height must be an integer")
         if value <= 0:
             raise ValueError("height must be > 0")
@@ -44,13 +55,12 @@ class Rectangle(Base):
 
     @property
     def x(self):
-        """ returns the x coordinate """
+        """Set/get the x coordinate of the Rectangle."""
         return self.__x
 
     @x.setter
     def x(self, value):
-        """ documents attribute """
-        if not isinstance(value, int):
+        if type(value) != int:
             raise TypeError("x must be an integer")
         if value < 0:
             raise ValueError("x must be >= 0")
@@ -58,103 +68,20 @@ class Rectangle(Base):
 
     @property
     def y(self):
-        """ returns the y coordinate """
+        """Set/get the y coordinate of the Rectangle."""
         return self.__y
 
     @y.setter
     def y(self, value):
-        """ documents attribute """
+        if type(value) != int:
+            raise TypeError("y must be an integer")
         if value < 0:
             raise ValueError("y must be >= 0")
         self.__y = value
 
     def area(self):
-        """ returns the area of the rectangle """
-        return self.__width * self.__height
-
-    def display(self):
-        """ prints in stdout the rectangle with the character # """
-        for i in range(self.__y):
-            print()
-        for i in range(self.__height):
-            for j in range(self.__x):
-                print(" ", end="")
-            for j in range(self.__width):
-                print("#", end="")
-            print()
-    #!/usr/bin/python3
-""" module for creating and updating objects from different sources """
-from models.base import Base
-
-
-class Rectangle(Base):
-    """
-    arguments for creating and updating objects from different sources
-    """
-    def __init__(self, width, height, x=0, y=0, id=None):
-        self.__width = width
-        self.__height = height
-        self.__x = x
-        self.__y = y
-        super().__init__(id)
-
-    @property
-    def width(self):
-        """ returns the width of attribute"""
-        return self.__width
-
-    @width.setter
-    def width(self, value):
-        """ documents attribute """
-        if not isinstance(value, int):
-            raise TypeError('width must be an integer')
-        if value <= 0:
-            raise ValueError('width must be > 0')
-        self.__width = value
-
-    @property
-    def height(self):
-        """ returns the height of attribute """
-        return self.__height
-
-    @height.setter
-    def height(self, value):
-        """ documents attribute """
-        if not isinstance(value, int):
-            raise TypeError("height must be an integer")
-        if value <= 0:
-            raise ValueError("height must be > 0")
-        self.__height = value
-
-    @property
-    def x(self):
-        """ returns the x coordinate """
-        return self.__x
-
-    @x.setter
-    def x(self, value):
-        """ documents attribute """
-        if not isinstance(value, int):
-            raise TypeError("x must be an integer")
-        if value < 0:
-            raise ValueError("x must be >= 0")
-        self.__x = value
-
-    @property
-    def y(self):
-        """ returns the y coordinate """
-        return self.__y
-
-    @y.setter
-    def y(self, value):
-        """ documents attribute """
-        if value < 0:
-            raise ValueError("y must be >= 0")
-        self.__y = value
-
-    def area(self):
-        """ returns the area of the rectangle """
-        return self.__width * self.__height
+        """Return the area of the Rectangle."""
+        return self.width * self.height
 
     def display(self):
         """Print the Rectangle using the `#` character."""
