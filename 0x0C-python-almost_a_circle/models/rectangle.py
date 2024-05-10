b9be6ba02835dc3419c3f88d9c43682468e7078d
bouzagui <badrbouzagui@gmail.com>
2024-05-10 12:34:18 +0100

    Done ✅

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -6,16 +6,11 @@ from models.base import Base
 class Rectangle(Base):
     """Rectangle"""
     def __init__(self, width, height, x=0, y=0, id=None):
-        if not isinstance(width, int) or width <= 0:
-            raise TypeError("width must be an integer")
-        elif not isinstance(height, int) or height <= 0:
-            raise TypeError("height must be an integer")
-        self.__width = width
-        self.__height = height
-        self.__x = x
-        self.__y = y
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
         super().__init__(id)
-
     @property
     def width(self):
         """get the width"""
@@ -54,7 +49,7 @@ class Rectangle(Base):
         """set x value"""
         if not isinstance(value, int):
             raise TypeError("x must be an integer")
-        if value <= 0:
+        if value < 0:
             raise ValueError("x must be >= 0")
         self.__x = value
 
@@ -71,3 +66,16 @@ class Rectangle(Base):
         if value < 0:
             raise ValueError("y must be >= 0")
         self.__y = value
+
+    def area(self):
+        """ fuctorial of width and height """
+        return self.__width * self.__height
+
+    def display(self):
+        """print symbol #"""
+        for i in range(self.height):
+            print("#" * self.width)
+
+    def __str__(self):
+        """update rectangle"""
+        return (f"[Rectangle] ({self.id}) {self.x}/{self.y} - {self.width}/{self.height}")
