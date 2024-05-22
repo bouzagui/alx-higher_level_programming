7b17df3d6f3dc1f09242454f1ac07faef1803a3d
bouzagui <badrbouzagui@gmail.com>
2024-05-23 00:32:11 +0100

    Done

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,4 @@
--- list all privileges of the MySQL users user_0d_1 and user_0d_2
-SHOW GRANTS FOR 'user_0d_1'@'localhost'
-SHOW GRANTS FOR 'user_0d_2'@'localhost'
+-- list all privileges of the MySQL user user_0d_1
+-- list all privileges of the MySQL user user_0d_2
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
