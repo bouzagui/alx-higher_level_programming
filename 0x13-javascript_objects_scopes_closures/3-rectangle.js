7d44dc5f4cc4079265a0a87c3e909138667ae8e9
bouzagui <badrbouzagui@gmail.com>
2024-06-20 00:20:58 +0100

    Done ✅

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,16 +1,16 @@
 #!/usr/bin/node
 class Rectangle {
-  constructor (h, w) {
-    if (h > 0 && w > 0) {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
       this.width = w;
       this.height = h;
     }
   }
 
   print () {
-    if (this.width && this.height) {
-      for (let i = 0; i < this.width; i++) {
-        console.log('X'.repeat(this.height));
+    if (!isNaN(this.width) && !isNaN(this.height)) {
+      for (let i = 0; i < this.height; i++) {
+        console.log('X'.repeat(this.width));
       }
     }
   }
