34fbd7f1a8106c2f03190d015ba2e4e3c264257d
bouzagui <badrbouzagui@gmail.com>
2024-06-20 01:31:41 +0100

    Done ✅

---
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const rectangle = require('./4-rectangle');
+
+class square extends rectangle {
+    constructor (size) {
+        super(size, size);
+    }
+}
+module.exports = square;
