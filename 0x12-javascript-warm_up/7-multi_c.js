e788960d8a78c8149540be3b50bff06b2ea49fe5
bouzagui <badrbouzagui@gmail.com>
2024-06-20 20:23:09 +0100

    Done ✅

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const Str = String('C is fun');
+const number = process.argv[2];
+if (isNaN(number)) {
+  console.log('not argument');
+} else {
+  for (let i = 0; i < number; i++) {
+    console.log(Str.replace(i));
+  }
+}
