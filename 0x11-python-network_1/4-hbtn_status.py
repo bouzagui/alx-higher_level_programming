4c660b78c70ff26cb1c57d81e3a1e5bc81aec4e6
bouzagui <badrbouzagui@gmail.com>
2024-07-06 18:23:11 +0100

    done ✅

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+import requests
+
+
+if __name__ == "__main__":
+    url = "https://alx-intranet.hbtn.io/status"
+    get_con = requests.get(url)
+    print("Body response:")
+    print("\t- type: {}".format(type(get_con.text)))
+    print("\t- content: {}".format(get_con.text))
