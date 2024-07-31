48c92d397831bc43062a85ccf03c67e2fb2b86c0
bouzagui <badrbouzagui@gmail.com>
2024-07-31 03:47:50 +0100

    done ✅

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,1 +1,12 @@
 #!/usr/bin/node
+const request = require('request');
+const url = process.argv[2];
+
+request(url, (err, response, body) => {
+  if (err) {
+    console.log(err);
+  }
+
+  const moveId = body.split('people/18/').length - 1;
+  console.log(moveId);
+});
--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,1 +1,19 @@
 #!/usr/bin/node
+
+const url = process.argv[2];
+const filename = process.argv[3];
+const request = require('request');
+const fs = require('fs');
+
+request(url, function (error, repsonse, body) {
+  if (error) {
+    console.log(error);
+  }
+  const data = body;
+  fs.writeFile(filename, data, { encoding: 'utf-8' }, function (err) {
+    if (err) {
+      console.log(err);
+      process.exit(1);
+    }
+  });
+});
